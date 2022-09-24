346beb3ab2c3cc4ed8eb9ffd40e3a89ec51aa28c
Matthew Mulford <mhmulford0@gmail.com>
2022-09-24 16:35:02 -0500

    init and update workflow

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,38 @@
-console.log('This is what would run if your app gets started.');
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+import {ethers} from 'ethers';
+
+import oracleAbi from './abi/oracle.json';
+import traitInfo from './nouns-image-data.json';
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+// const signer = provider.getSigner();
+
+const CONTRACT_ADDRESS = '0x6c3810649c140d2f43Ec4D88B2f733e1375E4C74';
+const oracleContract = new ethers.Contract(CONTRACT_ADDRESS, oracleAbi, provider);
+
+const main = async () => {
+	provider.on('block', async () => {
+		const blockNum = await provider.getBlockNumber();
+
+		const returnObj = {
+			background: 0,
+			blockNumber: blockNum,
+			body: 0,
+			glasses: 0,
+			head: 0,
+		};
+		const nextLil = await oracleContract.fetchNextNoun({blockTag: 'pending'});
+
+		returnObj.background = nextLil[4].background;
+		returnObj.body = nextLil[4].body;
+		returnObj.glasses = nextLil[4].glasses;
+		returnObj.head = nextLil[4].head;
+		const test = traitInfo.images.heads.at(nextLil[4].head);
+		console.log(test?.filename);
+	});
+};
+
+void main().then().catch();
